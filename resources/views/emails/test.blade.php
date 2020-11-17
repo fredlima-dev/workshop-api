674c62b967a2174fb918ffa2b8b21f5435fd4477
João Pedro Amaral Dias <joaopedrodiasamaral@gmail.com>
2020-11-17 09:19:37 -0300

    ajuste email recuperação de senha

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,3 +1,3 @@
 <h2>
-    Olá {{$data->name}}, link para gerar <a href="http://localhost:3000/{{$data->token_reset_password}}">nova senha</a>
+    Olá {{$data->name}}, link para gerar <a href="http://127.0.0.1:3000/reset-password?t={{$data->token_reset_password}}">nova senha</a>
 </h2>
