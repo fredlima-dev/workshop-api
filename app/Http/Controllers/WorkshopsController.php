e91142f0f0d0f2d8b214be3eb1711f12e237199c
Wesley Vinicius Santos <wv.vinicius@gmail.com>
2020-11-20 14:54:00 -0300

    Implementada a listagem de oficinas por id, e cadastro de nova oficina

--- app/Http/Controllers/WorkshopsController.php
+++ app/Http/Controllers/WorkshopsController.php
@@ -4,15 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Models\Workshop;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WorkshopsController extends Controller
 {
     
     
+    public function getListWorkshopId(Request $request){
+        $id = $request->route('id');
+
+        $workshop = DB::table('workshops')
+                                            ->where('id', $id)
+                                            ->first();
+
+        return response()->json($workshop);
+
+        // if($worklist == false){
+
+        //     return response()->json(
+        //         [
+        //             'message' => 'Ainda não existem oficinas cadastradas'
+        //         ]         
+        //     );
+        // }else{
+        //     return response()->json($worklist);
+        // }
+    }
+
     public function getListWorkshop(){
 
-        $worklist = Workshop::all();
-        if($worklist->empty()){
+        $worklist = DB::table('workshops')->orderBy('date', 'desc')->get();
+        if($worklist == false){
 
             return response()->json(
                 [
@@ -22,6 +44,26 @@ class WorkshopsController extends Controller
         }else{
             return response()->json($worklist);
         }
+    }
+
+    public function createWorkshop(Request $request){
+        $data = $request->only('name', 'panelist', 'date', 'duration', 'subscribers', 'detailsLink');
+
+        $validator = Workshop::validate($data);
+
+        if($validator->fails()){
+            return response()->json([
+                'error' => 'Dados inválidos',
+                'teste' => $data,
+                'data' => $validator->fails()
+            ], 400);
+        }
+
+        $workshop = Workshop::create($data);
+        return response()->json([
+            "message" => "Gravado com sucesso",
+            "data"=> $workshop            
+        ]);
     }
     
 }
